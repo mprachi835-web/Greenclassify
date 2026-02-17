76a4a54eb08c1e0340efaec2870c64db3d59f8d9
Prachi472 <mprachi835@gmail.com>
2026-02-17 20:42:43 +0530

    first commit

---
+++ flask/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4467e6e8f560ce7a25e21a685b4c1f8e2591c7992e68131341a4695a31b4bbbe
+size 2453
---
+++ flask/static/js/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c568c53c15f27f2440f6e791966852a6afc828454c739f0b53afdae3041ffb3
+size 6736
